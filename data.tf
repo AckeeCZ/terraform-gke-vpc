b41b7a51d72895c1b54c239226bebee8056e4fd7
Tomáš Hejátko <tomas.hejatko@gmail.com>
2019-10-17 12:52:35 +0200

    no special characters in cluster_password. See https://github.com/kubernetes/kubernetes/issues/65633

--- data.tf
+++ data.tf
@@ -5,14 +5,12 @@ data "google_container_engine_versions" "current" {
 
 resource "random_string" "cluster_password" {
   length           = 16
-  special          = true
-  override_special = "/@\" "
+  special          = false
 }
 
 resource "random_string" "cluster_username" {
   length           = 16
   special          = false
-  override_special = "/@\" "
 }
 
 data "google_compute_network" "default" {
