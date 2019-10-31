e6614b3c92c019663a9486345e83d5675715617b
Sandra Tatarevićová <sandra.tatarevicova@ackee.cz>
2019-10-31 12:05:52 +0100

    Allow assigning external IP addresses to cluster's nodes. See RM #39227.

--- main.tf
+++ main.tf
@@ -44,8 +44,7 @@ resource "google_container_cluster" "primary" {
 
   private_cluster_config {
     enable_private_endpoint = false
-    enable_private_nodes    = true
-    master_ipv4_cidr_block  = "172.16.0.0/28"
+    enable_private_nodes    = false
   }
 
   ip_allocation_policy {}
